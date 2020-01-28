9b3eeb17d4c8c64c638199b546eb9a616c20989b
surelis segarra <sdarianne34@gmail.com>
2020-01-28 11:50:55 -0800

    added data

---
+++ api.js
@@ -0,0 +1,123 @@
+export const productData = [
+    {
+        id:'bag',
+        name:'Star Wars Looking Bag',
+        image:'assets/bag.jpg',
+        description: `Don't cry. Buy this bag and get over it.`
+    },
+    {
+        id:'banana',
+        name:'Bana Slicer',
+        image:'assets/banana.jpg',
+        description: 'Stop trying to come up with an ideal way to slice a banana. we got you. '
+    },
+    {
+        id:'bathroom',
+        name:'toilet paper holder with ipad stand',
+        image:'assets/bathroom.jpg',
+        description:'iPad Pedestal Stand with toilet paper holder is a must have.'  
+    },
+    {
+        id:'boots',
+        name:'Open front boots',
+        image:'assets/boots.jpg',
+        description:'This are definetely the best choice for those rainy days.'
+    },
+    {
+        id:'breakfast',
+        name:'oven/stove/coffee maker thingy',
+        image:'assets/breakfast.jpg',
+        description:'the best choice in the morning.'
+    },
+    {
+        id:'bubblu-gum',
+        name:'Meatball BumbleGum ',
+        image:'assets/bubblegum.jpg',
+        description:'fully cooked and ready to chew'
+    },
+    {
+        id:'chair',
+        name:'The most comfortable chair in the world.',
+        image:'assets/chair.jpg',
+        description:'the best thing for your back.'
+    },
+    {
+        id:'alien',
+        name:'Some weird Alien with a guy in his hands',
+        image:'assets/cthulhu.jpg',
+        description:'somethings cheap we got from Interdimensional Cable.'
+    },
+    {
+        id:'dog duck',
+        name:'A Duck',
+        image:'assets/dog-duck.jpg',
+        description:'Is a duck. Sometimes barks, but is because i bilingual.'
+    },
+    {
+        id:'dragon',
+        name:'Dragon Meat',
+        image:'assets/dragon.jpg',
+        description:'The most tender meat you woudl ever have.'
+    },
+    {
+        id:'pen',
+        name:'Pen-fork',
+        image:'assets/pen.jpg',
+        description:'perfect to eat your favorite song and write your best ideas.'
+    },
+    {
+        id:'pet-sweep',
+        name:'Pet Sweep',
+        image:'assets/pet-sweep.jpg',
+        description:'From now on, you dont have to clean his mess.'
+    },
+    {
+        id:'scissors',
+        name:'Scissor And Pizza Holder',
+        image:'assets/scissors.jpg',
+        description:`perfect for eating pizza while you cry and cut your ex's photos`
+    },
+    {
+        id:'shark',
+        name:'Shark Blanket',
+        image:'assets/shark.jpg',
+        description:'baby shark do do do .'
+    },
+    {
+        id:'sweep',
+        name:'Sweep',
+        image:'assets/sweep.jpg',
+        description:'I mean, your paying for his college. Least thing he can do is clean.'
+    },
+    {
+        id:'tauntaun',
+        name:'Some Tauntaun Thing, IDK',
+        image:'assets/tauntaun.jpg',
+        description:'This generation like weird things, idk.'
+    },
+    {
+        id:'unicorn',
+        name:'Unicorn Meat',
+        image:'assets/unicorn.jpg',
+        description:'Better than steak.'
+    },
+    {
+        id:'usb',
+        name:'Tentacles USB',
+        image:'assets/usb.jpg',
+        description:'Spend money on this, come on.'
+    },
+    {
+        id:'water-can',
+        name:'Water Ccan',
+        image:'assets/water-can.jpg',
+        description:'best things for your flowers.'
+    },
+    {
+        id:'wine-glass',
+        name:'Wine Glass',
+        image:'assets/wine-glass.jpg',
+        description:'For all thos wine lovers.'
+    }
+
+]
